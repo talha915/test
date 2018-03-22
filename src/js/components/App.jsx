3845478948dc65c57ec8c5e13cda55a8a4016ef6
Talha <talha915426@gmail.com>
2018-03-22 16:11:14 +0500

    Reactjs deployed

---
+++ src/js/components/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div className="test1">
+                Test
+                <div className="test">
+                    Test1
+                </div>
+            </div>
+        );
+    }
+}
